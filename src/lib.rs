4186b5b5bbd1b52c6276b97ea3e8ed23dfa6676b
EthanWakeford <ethanwakeford@gmail.com>
2024-09-21 21:38:40 -0500

    chapter 4.4

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,106 @@
-#[derive(Debug, PartialEq)]
-pub struct Number(pub i32);
+use std::{
+    fmt, fs,
+    io::{self, Write},
+    path::Display,
+};
 
-impl Number {
-    pub fn new(s: &str) -> Self {
-        Self(s.parse().unwrap())
-    }
+enum TokenType {
+    // Single-character tokens.
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
+
+    // One or two character tokens.
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+
+    // Literals.
+    Identifier,
+    String,
+    Number,
+
+    // Keywords.
+    And,
+    Class,
+    Else,
+    False,
+    Fun,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Let,
+    While,
+
+    EOF,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+struct Token {
+    tokentype: TokenType,
+    lexeme: String,
+    // No idea what this should actually be
+    // book has it as object
+    literal: String,
+    line: i32,
+}
 
-    #[test]
-    fn parse_number() {
-        assert_eq!(Number::new("123"), Number(4));
+// impl fmt::Display for Token {
+//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+//         write!(
+//             f,
+//             "({}, {}, {}, {})",
+//             self.tokentype, self.lexeme, self.literal, self.line
+//         )
+//     }
+// }
+
+fn scan(input: &String) {
+    let had_error = false;
+
+    println!()
+}
+
+pub fn run_file(filename: &String) -> Result<(), io::Error> {
+    println!("im a runnin {}", filename);
+    let contents = fs::read_to_string(filename)?;
+
+    scan(&contents);
+    Ok(())
+}
+
+pub fn run_proompt() -> Result<(), io::Error> {
+    println!("im a proomptin");
+    loop {
+        print!("  ===]> ");
+
+        std::io::stdout().flush().unwrap();
+        let mut buffer = String::new();
+        io::stdin().read_line(&mut buffer)?;
+
+        let mut inputs = buffer.split_whitespace();
+
+        let command = match inputs.next() {
+            Some(s) => s,
+            None => continue,
+        };
     }
 }
---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use std::env;
+
+use rox::{run_file, run_proompt};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    println!();
+
+    if args.len() > 2 {
+        println!("Usage: rox [script]")
+    } else if args.len() == 2 {
+        let filename = args.get(1).expect("should be filename to unwrap");
+
+        if let Err(e) = run_file(filename) {
+            eprintln!("Error: {}", e);
+        }
+    } else {
+        if let Err(e) = run_proompt() {
+            eprintln!("Error: {}", e);
+        };
+    }
+}
